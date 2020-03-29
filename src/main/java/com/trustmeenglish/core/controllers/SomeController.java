5f7f798b2a5ad35b2189b31a76ca49892bdd5d6b
Nemo <hackersaheadddd@gmail.com>
2020-03-29 21:38:59 +0300

    first

---
+++ src/main/java/com/trustmeenglish/core/controllers/SomeController.java
@@ -0,0 +1,12 @@
+package com.trustmeenglish.core.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeController {
+    @GetMapping("/hello")
+    public String sayHello(){
+      return "Hello world";
+    }
+}
